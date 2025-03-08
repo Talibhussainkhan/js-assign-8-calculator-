bf02cef007a29920853390938198e7f42fa5c67a
Talib Hussain Khan <talibhk161@gmail.com>
2025-03-08 22:02:57 +0500

    added backspace funtionality

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ let operator = ["+", "-", "/", "*"]
 
 const abc = (a)=>{
     let lastChar = input.value.slice(-1);
-    // console.log(operator.indexOf(lastChar))
   if(a === "="){
     input.value = eval(input.value)
   }else if(operator.indexOf(lastChar) !== -1 && operator.indexOf(a) !== -1){
     input.value = input.value.slice(0, -1) + a ;
+  }else if(a === "del"){
+    input.value = input.value.slice(0, -1)
   }
-  else if(a === "del"){
+  else if(a === "C"){
     input.value = ""
   }
   else{
